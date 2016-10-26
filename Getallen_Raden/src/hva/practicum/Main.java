59a8ec1c57ff957c1b0c99cc0e0d271c211227be
SoaringDylan <dylan@fampiera.nl>
2016-10-26 17:34:49 +0200

    Comments

--- Getallen_Raden/src/hva/practicum/Main.java
+++ Getallen_Raden/src/hva/practicum/Main.java
@@ -1,6 +1,5 @@
 package hva.practicum;
 
-import java.util.IntSummaryStatistics;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,18 +10,19 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int[] getallen = genereerGetallen();
+        int[] getallen = genereerGetallen(); //Generates 3 different numbers
 
-        int guesses = 0;
-        int correct = 0;
+        int guesses = 0; //Keeps track on how many guesses the user requires.
+        int correct = 0; //Keeps track on how many correct numbers the user guessed in one query
+
         boolean num1, num2, num3, isCorrect = false;
         do {
             guesses++;
-            int[] input = getInput();
+            int[] input = getInput(); //Asks the user for 3 numbers seperated by a space
 
 
             num1 = komtVoorIn(input[0],getallen);
-            num2 = komtVoorIn(input[1],getallen);
+            num2 = komtVoorIn(input[1],getallen); //Checks if the users input is the same as one of the 3 randomly generated numbers in "getallen"
             num3 = komtVoorIn(input[2],getallen);
 
 
@@ -32,6 +32,7 @@ public class Main {
                 if (num2) correct += 1;
                 if (num3) correct += 1;
                 if (num1 != true && num2 != true && num3 != true) correct = 0;
+
                 System.out.println("Aantal correcte getallen = " + correct + "\n");
             }
         } while(isCorrect == false);
@@ -42,14 +43,14 @@ public class Main {
     }
 
 
-    public static int[] genereerGetallen() {
+    public static int[] genereerGetallen() { //Generates 3 random numbers, which must be different from eachother
         int[] getallen = new int[3];
 
         getallen[0] = (rand.nextInt(10)+1);
 
         do{
             getallen[1] = (rand.nextInt(10)+1);
-        } while (getallen[0] == getallen[1]);
+        } while (getallen[0] == getallen[1]); //The while statements make sure no double numbers appear in the system.
         do{
             getallen[2] = (rand.nextInt(10)+1);
         } while (getallen[0] == getallen[2] || getallen[1] == getallen[2]);
@@ -66,7 +67,7 @@ public class Main {
     public static int[] getInput() {
         System.out.print("Geef 3 verschillende getallen tussen 1 en 10, gescheiden door spaties: ");
         String numbers = scan.nextLine();
-        String[] sep = numbers.split(" ");
+        String[] sep = numbers.split(" "); //The array sep will be created with all numbers separated by a space.
 
         int[] zoekgetallen = new int[3];
 
